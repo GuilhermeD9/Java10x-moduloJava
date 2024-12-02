4cdf3d4c2a791e13de1fc81e2eebec303cef1873
Guilherme Silva <sgsouza.173@gmail.com>
2024-12-02 09:56:09 -0300

    Aula 02 ShortCuts

--- src/TipoDeDados/DadosPrimitivos.java
+++ src/TipoDeDados/DadosPrimitivos.java
@@ -7,5 +7,11 @@ public class DadosPrimitivos {
         char inicial = 'N';
         boolean vivoOuMorto = true;
         Long saldoBancario = 99999L; //Valor máximo: 9,233,372,036,854,775,807
+
+        System.out.println(idade); // Comando para mostrar a idade do usuário
+        System.out.println(saldoBancario);
+        System.out.println(altura);
+        System.out.println("Saldo Bancario é = " + saldoBancario);
+        System.out.println("Minha idade é: " + idade);
     }
 }
