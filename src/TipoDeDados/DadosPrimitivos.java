cc1f9beea14cb642b9bfa759b44619b4fa39dee5
Guilherme Silva <sgsouza.173@gmail.com>
2024-12-02 09:35:28 -0300

    Aula01
    Dados primitivos

---
+++ src/TipoDeDados/DadosPrimitivos.java
@@ -0,0 +1,11 @@
+package TipoDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+        int idade = 16; // Valor máximo: 2147483647
+        double altura = 1.65;
+        char inicial = 'N';
+        boolean vivoOuMorto = true;
+        Long saldoBancario = 99999L; //Valor máximo: 9,233,372,036,854,775,807
+    }
+}
