815b9cbfdbc18abd53e06664cefd7063a20f0b8c
Guilherme Silva <sgsouza.173@gmail.com>
2024-12-03 10:20:38 -0300

    ternarios

---
+++ src/Condicoes/Ternarios.java
@@ -0,0 +1,15 @@
+package Condicoes;
+
+public class Ternarios {
+    public static void main(String[] args) {
+        /*
+        * Ternários: Maneiras de reduzir o código
+        * variavel - (condicao) ? valorSeVerdadeiro : valorSeFalso;
+         */
+
+        short numeroDeMissoes = 1;
+        String nivel = (numeroDeMissoes >= 10) ? "Esse ninja está com mais de 10 missões" : "Esse ninja tem menos de 10 missões";
+        System.out.println(nivel);
+
+    }
+}
