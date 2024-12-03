f7664f0c453548cc9d50cd7271e088835efb5904
Guilherme Silva <sgsouza.173@gmail.com>
2024-12-03 17:13:34 -0300

    Garbage collector + array

--- src/NivelBasico/Array.java
+++ src/NivelBasico/Array.java
@@ -12,6 +12,21 @@ public class Array {
         System.out.println(ninja[5]);
         System.out.println(ninja);
 
+        // Redeclarar o Array
+        ninja = new String[7];
+        ninja[0] = "Hashirama Senju";
+        ninja[1] = "Tobirama Senju";
+        ninja[2] = "Hiruzen Sarutobi";
+        ninja[3] = "Minato Namikaze";
+        ninja[4] = "Tsunade";
+        ninja[5] = "Kakashi Hatake";
+        ninja[6] = "Naruto Uzumaki";
+        System.out.println(ninja[2]);
+
+        for (int i = 0; i < 7; i++) {
+            System.out.println(ninja[i]);
+        }
+
         // Int inicializam como 0
         int[] idade = new int[2];
         System.out.println(idade[1]);
@@ -27,7 +42,8 @@ public class Array {
         System.out.println(flutuante[0]);
         System.out.println(flutuante);
 
-        String nomeDoNinja1 = "Gaara do deserto";
-        String nomeDoNinja2 = "Rock lee";
+        String nomeDoNinja1 = "Naruto Uzumaki";
+        nomeDoNinja1 = "Boruto Uzumaki";
+        System.out.println(nomeDoNinja1);
     }
 }
