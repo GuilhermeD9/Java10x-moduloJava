d984e7aa099f44888c02d7c997e0a10a566eb312
Guilherme Silva <sgsouza.173@gmail.com>
2024-12-05 11:46:10 -0300

    comparacao Ca vs In

---
+++ src/NivelIntermediario/ClasseAbVsInterface/Main.java
@@ -0,0 +1,15 @@
+package NivelIntermediario.ClasseAbVsInterface;
+
+public class Main {
+    public static void main(String[] args) {
+        Uzumaki Naruto = new Uzumaki();
+        //Naruto.nome = "Naruto Uzumaki";
+        Naruto.tacarUmShuriken();
+
+        Uchiha Sasuke = new Uchiha();
+        Sasuke.nome = "Sasuke Uchiha";
+        Sasuke.nomeDoNinja();
+        Sasuke.tacarKunai();
+
+    }
+}
