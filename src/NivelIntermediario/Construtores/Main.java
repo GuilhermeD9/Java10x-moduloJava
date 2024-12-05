3fba3b396be979cbbe5e3a704489eb1f448f86fb
Guilherme Silva <sgsouza.173@gmail.com>
2024-12-05 11:16:05 -0300

    classes abstratas

--- src/NivelIntermediario/Construtores/Main.java
+++ src/NivelIntermediario/Construtores/Main.java
@@ -2,18 +2,11 @@ package NivelIntermediario.Construtores;
 
 public class Main {
     public static void main(String[] args) {
-        Hokages hashirama = new Hokages();
+
+        Senju hashirama = new Senju();
+        hashirama.nome = "Hashirama Senju";
         hashirama.idade = 45;
+        hashirama.sabedoriaHokage();
 
-        //Hokages tobirama = new Hokages("Tobirama Senju");
-        //System.out.println(tobirama.nome);
-
-        Hokages hiruzen = new Hokages();
-        System.out.println(hiruzen.idade);
-
-        Hokages minato = new Hokages("Minato Namikaze", 32, false, "Da folha", 85, 724, 1.75);
-        System.out.println(minato.nome);
-
-        Hokages tsunade = new Hokages();
     }
 }
---
+++ src/NivelIntermediario/Construtores/Senju.java
@@ -0,0 +1,7 @@
+package NivelIntermediario.Construtores;
+
+public class Senju extends Hokages{
+    public void sabedoriaHokage() {
+        System.out.println("Você ganhou sabedoria!");
+    }
+}
