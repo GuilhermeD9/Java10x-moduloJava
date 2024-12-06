ff9f823c0fa572238034a5111573faba65e5c258
Guilherme Silva <sgsouza.173@gmail.com>
2024-12-06 11:42:46 -0300

    overload metodos e construtores

--- src/NivelIntermediario/Overload/Main.java
+++ src/NivelIntermediario/Overload/Main.java
@@ -17,11 +17,16 @@ public class Main {
         // Obj Uchiha 2®
         Uchiha itachi = new Uchiha("Itachi Uchiha", "Aldeia da folha", 27 );
         itachi.habilidadeEspecial();
+        itachi.metodoProvisorios();
 
         // obj Uchiha 3
         Uchiha madara = new Uchiha("Madara Uchiha", "Aldeia da folha", 45,900, NivelNinja.KAGE);
         madara.habilidadeEspecial();
         madara.estrategiaDeBatalhaNinja();
+        // Método comum
+        madara.inteligenciaDeCombate();
+        // Método sobrecarregado
+        madara.inteligenciaDeCombate(90);
 
 
 
--- src/NivelIntermediario/Overload/Uchiha.java
+++ src/NivelIntermediario/Overload/Uchiha.java
@@ -14,10 +14,32 @@ public class Uchiha extends Ninja {
         super(nome, aldeia, idade, numeroDeMissoesConcluidas, rank);
     }
 
-    // Sobreescrever o metodo da classe Ninja
+
     @Override
     public void habilidadeEspecial() {
         System.out.println("Meu nome é " + nome + " e esse é meu ataque Uchiha, um ataque de fogo. Eu ja completei: " + numeroDeMissoesConcluidas + " missoes." );
+    }
+
+    @Override
+    public void inteligenciaDeCombate() {
+        System.out.println("Meu nome é: " + nome + " Essa é minha inteligência de combate");
+    }
+
+    @Override
+    public void inteligenciaDeCombate(int qi) {
+        if (qi > 150) {
+            System.out.println("Seu QI é: " + qi + " e você é um gênio");
+        } else if (qi >= 130) {
+            System.out.println("Seu QI é: " + qi + " e você é um ninja promissor");
+        } else {
+            System.out.println("Seu QI é: " + qi + " e você precisa treinar mais suas estratégias.");
+        }
+    }
+
+    //Teste na classe uchiha
+    @Override
+    public void metodoProvisorio() {
+        System.out.println("Teste na classe Uchiha");
     }
 
 }
