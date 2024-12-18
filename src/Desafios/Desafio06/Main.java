1c2ef05775abab7030037ad5e4f2faabe4b74311
Guilherme Silva <sgsouza.173@gmail.com>
2024-12-18 09:44:48 -0300

    Desafio 06 Finalizado

--- src/Desafios/Desafio06/Main.java
+++ src/Desafios/Desafio06/Main.java
@@ -1,6 +1,9 @@
 package Desafios.Desafio06;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -26,6 +29,8 @@ public class Main {
         // TODO: Adicionar Ninja no início da lista
         listaDeNinjas.addFirst(new Ninja("Boruto Uzumaki", 9, "Konoha"));
 
+        System.out.println("------------- Removendo ninja --------------");
+
         // TODO: Remover Ninja no início
         System.out.println("Ninja removido = " + listaDeNinjas.removeFirst());
 
@@ -36,10 +41,46 @@ public class Main {
             System.out.println("Lista de Ninjas depois das mudanças = " + novaListaDeNinjas);
         }
 
+        System.out.println("------------- Ninja no index --------------");
+
         // TODO: Achar um ninja em um determinado index
         Ninja terceiroNinja = listaDeNinjas.get(3);
         System.out.println("Terceiro Ninja - " + terceiroNinja);
 
+        System.out.println("\n------------- Desafio Extra --------------");
 
+        // TODO: DESAFIO ADICIONAL: Implementar ordem por diferentes critérios
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escolha a comparação");
+        System.out.println("1 - Ordenar por nome");
+        System.out.println("2 - Ordenar por idade");
+        System.out.println("3 - Ordenar por vila");
+        System.out.print("Sua opção: ");
+        int opcao = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (opcao) {
+            case 1 :
+                Collections.sort(listaDeNinjas, Comparator.comparing(Ninja::getNome));
+                System.out.println("Ordenado por nome: " + listaDeNinjas);
+                break;
+            case 2 :
+                Collections.sort(listaDeNinjas, Comparator.comparing(Ninja::getIdade));
+                System.out.println("Ordenado por idade: " + listaDeNinjas);
+                break;
+            case 3 :
+                Collections.sort(listaDeNinjas, Comparator.comparing(Ninja::getVila));
+                System.out.println("Ordenado por vila: " + listaDeNinjas);
+                break;
+            default:
+                System.out.println("Suas opções são de 1 a 3! Corrija sua digitação.");
+        }
+
+        // TODO: Buscar ninja com base no nome
+        for (Ninja ninja : listaDeNinjas) {
+            if (ninja.getNome().equals("Gaara")) {
+                System.out.println(ninja);
+            }
+        }
     }
 }
